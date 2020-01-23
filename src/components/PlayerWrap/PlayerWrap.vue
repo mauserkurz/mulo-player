5244d6ac68c829de6d3ce07b47ee006ffe47b24a
qwerty <mauserkurz98@gmail.com>
2020-01-24 05:06:12 +0800

    player prototype

--- src/components/PlayerWrap/PlayerWrap.vue
+++ src/components/PlayerWrap/PlayerWrap.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="player-wrap">
-    <img alt="App logo" src="../../assets/logo.svg">
-    <Player/>
+    <img alt="App logo" src="../../assets/logo.svg" width="256" height="256">
+    <Player file="URL"/>
   </div>
 </template>
 
@@ -11,6 +11,10 @@ import Player from '@/views/Player/Player.vue';
 export default {
   name: 'Auth',
   components: { Player },
+
+  data() {
+    return { URL: '' };
+  },
 };
 </script>
 
