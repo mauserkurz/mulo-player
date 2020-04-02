e4770bb17860bdbf0e0fd2afa28a47cfa924739b
qwerty <mauserkurz98@gmail.com>
2020-04-03 06:10:55 +0800

    node server

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('express')();
+const path = require('path');
+const serveStatic = require('serve-static');
+const PORT = process.env.PORT || 5000;
+
+app.use(serveStatic(__dirname + "/dist"));
+app.listen(PORT);
+
+console.log('server started '+ PORT);
