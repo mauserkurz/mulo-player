e30afc08d6d46cceb5c83c68261736e75fcff591
qwerty <mauserkurz98@gmail.com>
2020-05-03 14:30:04 +0800

    reduce service-worker loggers

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,32 +1,26 @@
-/* eslint-disable no-console */
 import { register } from 'register-service-worker';
 import utils from '@/utils';
 
 if (utils.isProd()) {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
-      console.log(
-        'App is being served from cache by a service worker.\n'
-        + 'For more details, visit https://goo.gl/AFskqB',
+    ready(serviceWorkerRegistration) {
+      const delimiter = () => window.console.log(
+        '%c<================================>',
+        'color:#009688;font-weight:900;font-size:40px',
       );
+      delimiter();
+      window.console.log(
+        '%cApp is being served from cache by a service worker.\n'
+        + '%cFor more details, visit https://goo.gl/AFskqB',
+        'color:#000;font-weight:900;font-size:20px',
+        'color:#2196F3;font-weight:400;font-size:15px',
+      );
+      window.console.table(serviceWorkerRegistration);
+      delimiter();
     },
-    registered() {
-      console.log('Service worker has been registered.');
-    },
-    cached() {
-      console.log('Content has been cached for offline use.');
-    },
-    updatefound() {
-      console.log('New content is downloading.');
-    },
-    updated() {
-      console.log('New content is available; please refresh.');
-    },
-    offline() {
-      console.log('No internet connection found. App is running in offline mode.');
-    },
+
     error(error) {
-      console.error('Error during service worker registration:', error);
+      window.console.error('fetch service worker failed => ', error);
     },
   });
 }
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,26 @@ module.exports = {
   devServer: {
     proxy: 'http://localhost:3000',
   },
+  pwa: {
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      navigateFallback: 'index.html',
+      exclude: [
+        /\.map$/,
+        /manifest\.json$/,
+      ],
+    },
+    themeColor: '#009688',
+    msTileColor: '#000',
+    appleMobileWebAppCapable: 'no',
+    appleMobileWebAppStatusBarStyle: 'default',
+    manifestOptions: {},
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png',
+    },
+  },
 };
